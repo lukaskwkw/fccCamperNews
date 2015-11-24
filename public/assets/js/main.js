e47427ea309624310435a06e1dc159b2d5e13912
lukaskwkw <lukaskwkw@gmail.com>
2015-11-24 10:45:38 +0100

    layout added

---
+++ public/assets/js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+})
+
+/* 
+
+to-do
+timePosted decoded
+'posted' + hourly-time % 24 + 'day ago'
+
+headline: "Become a Programmer, Motherfucker",
+timePosted: 1448219443441,
+metaDescription: "",
+description: "undefined",
+rank: 13,
+
+author: {
+picture: "https://avatars.githubusercontent.com/u/449874?v=3",
+username: "storbeck"
+}
+image: "",
+
+*/

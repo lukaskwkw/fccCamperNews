ae1064b3135d88bc5fa325411f615e9beb389012
lukaskwkw <lukaskwkw@gmail.com>
2015-11-26 16:09:14 +0100

    finished v1

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,11 +5,6 @@ var url = 'http://www.freecodecamp.com/news/hot'
 
 $.getJSON(url,formatJson);
 
-
- $('.media-head').click(function(){
- 	$('.cssload-container').remove();
- 	$('.media-left').fadeIn(1000);
- })
 })
 
 
@@ -30,43 +25,90 @@ function formatJson(json)
 	var headline = json[i].headline;
 	var timePosted = json[i].timePosted;
 	var description = json[i].metaDescription;
-	var colSm = 4;
+	var link = json[i].link;
+	var headImage = json[i].image;
+	var colSm = 4; 
 	var styleProfilImage = '';
-	if (upVotes>=10 && upVotes<20)
-		colSm = 8;
-	if (upVotes>=20)
-		colSm = 12;
+	var colorStyle = '';
+	if (upVotes>=10 && upVotes<20){
+			colSm = 8;
+			colorStyle = 'orange';
+		}
+	if (upVotes>=20){
+			colSm = 12;
+			colorStyle = 'red';
+		}
 	if (Boolean(profilImage)===true)
-		styleProfilImage = "style=\'background-image: url(\""+profilImage+"\");\'"
+		styleProfilImage = "style=\'background-image: url(\""+profilImage+"\");\'";
 
-var itemHtml = '<div class="item col-sm-'+colSm+' col-xs-12">'+
-				'<div class="media-head">'
+	var timeNow = new Date();
+	var timeHtml = '';
+	timePosted = Math.abs(timeNow.getTime()-timePosted);
+	var diffrenceInHours = (timePosted/1000/3600);
+	if (diffrenceInHours>=48)
+	{	
+	timePosted = Math.floor(diffrenceInHours/24);
+	timeHtml = timePosted + ' days ago'
+	}
+	
+	if (diffrenceInHours>=24 && diffrenceInHours<48)
+	{	
+	timePosted = Math.floor(diffrenceInHours/24);
+	timeHtml = 'a day ago'
+	}
+	if (diffrenceInHours<24 && diffrenceInHours>1){
+	timePosted = Math.floor(diffrenceInHours);
+	timeHtml = timePosted + ' hours ago'
+	}
+	if (diffrenceInHours<=1)
+	{
+			timePosted = Math.floor(diffrenceInHours);
+			timeHtml = 'a hour ago'
+	}
+
+	var descriptionHtml = '';
+	if (Boolean(description)!==false)
+		descriptionHtml = '<div class="description"><p>'+description+'</p></div>';
+
+		styleBackgroundImage = "style=\'background-image: url(\""+headImage+"\");\'";
+		var divImage= "";
+		if (Boolean(headImage)===true)
+			divImage = '<div class="media-image" '+styleBackgroundImage+'></div>';
+			var imgAlternative = '<img class="media-image-alternative" src="'+headImage+'">'; //image with <img> tag
+// '<h3 class="pull-left"><span class="rank">5</span><i class="glyphicon glyphicon-fire red--hearth"></i></h3>'
+var itemHtml = '<div class="item col-sm-'+colSm+' col-xs-12">'
+				+'<div class="media-head">'
 				+'<div class="media-left"'+styleProfilImage+'>'
 				+'	</div>'
 				+'			<div class="media-headline">'
-				+'				<h4>'+headline+'</h4>'
-				+'			</div>'
+				+'				<a href="'+link+'"><h3 class="pull-left"><span class="rank">'+upVotes+'</span>'+
+						'<i class="glyphicon glyphicon-fire '+colorStyle+'"></i></h3><h4>'+headline+'</h4></a>'
+				+'			'+divImage+descriptionHtml+'</div>'
 				+'		<div class="media-byline">'
-				+'			<span class="media-byline__time">'+timePosted+'</span>'
+				+'			<span class="media-byline__time">'+timeHtml+'</span>'
 				+'				<span class="media-byline__author">by @'+username+'</span>'
-				+'			<div class="pull-right"><i class="glyphicon glyphicon-heart red--hearth"></i>'
-				+'		<span class="rank">'+upVotes+'</span>'
+				+'			<div class="pull-right">'
+				
 				+'		</div>'
 				+'			</div>'
 				+'		</div>'
-				+'		<div class="description">'
-				+'		<p>'+description+'</p>'
-				+'		</div>'
+				// +descriptionHtml
 				+'	</div>'
 
 
 	row.append(itemHtml);
 }
 
+ 	$('.wrap-load').fadeOut(1000, function() {
+ 		 	$('.wrap-load').remove();
+ 	});
+
 }
 
 
 /* 
+
+<img class="media-image" src="'+headImage+'">
 
 to-do
 timePosted decoded
@@ -83,5 +125,10 @@ picture: "https://avatars.githubusercontent.com/u/449874?v=3",
 username: "storbeck"
 }
 image: "",
+
+    background-image: url("https://i.kinja-img.com/gawker-media/image/upload/s--9L2JbiDq--/c_fill,fl_progressive,g_north,h_358,q_80,w_636/1350289244899804964.jpg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 300px;
 
 */
